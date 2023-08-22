05dce97b0bde22c3ffcd8677c2911ef2d46ae579
Mauricio CR <mauriciocr123@outlook.com>
2023-08-22 15:05:25 -0600

    KIN-184 Import the velzon template to the repository
    The Velzon Template has been added to have a template that allows to speed up the creation of an administration section.  In-depth, has been added some views, styles, libs and javascript to make correctly Velzon Template work on this project.

---
+++ app/Views/partials/head-css.php
@@ -0,0 +1,6 @@
+<!-- Layout config Js -->
+<script src="/assets/js/admin/layout.js"></script>
+<!-- Bootstrap Css -->
+<link href="/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<!-- App Css-->
+<link href="/assets/css/admin.min.css" rel="stylesheet" type="text/css" />
