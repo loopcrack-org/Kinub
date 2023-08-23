f6776b5aec11045d9768fa127ea56a86e9797003
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-08-23 12:11:22 -0600

    KIN-159 Corrections of the email controller and creation of routers
    The route was added to be able to make the post type request and change the error messages from English to Spanish

--- app/Controllers/CtrlEmail.php
+++ app/Controllers/CtrlEmail.php
@@ -8,14 +8,30 @@ class CtrlEmail extends BaseController
 {
     public function sendContactEmail(): RedirectResponse
     {
-        if($this->validate([
+        $validationRules = [
             "product-name" => "required",
             "inquirer-name" => "required|alpha_space",
             "inquirer-email" => "required|valid_emails",
             "message" => "required"
-        ])){
-            return redirect()->back();
-        }else{
+        ];
+        $validateMessage = [
+            "product-name" => [
+                "required" => "El nombre del producto es obligatorio"
+            ],
+            "inquirer-name" => [
+                "required" => "El nombre del solicitante es obligatorio",
+                "alpha_space" => "El campo nombre solo debe contener carácteres alfanuméricos y espacios"
+            ],
+            "inquirer-email" => [
+                "required" => "El email es obligatorio",
+                "valid_emails" => "El email debe contar con un formato valido"
+            ],
+            "message" => [
+                "required" => "El mensaje es obligatorio",
+            ],
+        ];
+
+        if(!$this->validate([$validationRules, $validateMessage])){
             return redirect()->back()->withInput()->with("errors", $this->validator->getErrors());
         }
     }
