fdae86bbd57f164ef1c97675b5731143b2b3b607
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-08-24 03:44:43 -0600

    KIN-80 form submission and error validation added
    Added code for error field validation and successful submission handling

--- app/Views/public/index.php
+++ app/Views/public/index.php
@@ -1,10 +1,43 @@
 <?php $this->extend('public/templates/layout'); ?>
 
+<!-- CSS -->
+<?php $this->section('css'); ?>
+<link rel="stylesheet" href="assets/css/public.min.css" type="text/css">
+<?php $this->endSection(); ?>
+
+<!-- JS -->
+<?php $this->section('js'); ?>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<?php $this->endSection(); ?>
+
+<!-- FONTS -->
+<?php $this->section('fonts'); ?>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Nunito+Sans:opsz@6..12&display=swap" rel="stylesheet">
+<?php $this->endSection(); ?>
+
 <?php $this->section('content'); ?>
+<?php $errors = session()->get('errors'); ?>
+
 <section id="form">
+    <?php 
+        $response = session()->get('response');
+        if(isset($response)):
+    ?>
+        <script type="module">
+            Swal.fire({
+                title: <?php echo $response['title']; ?>,
+                text: <?php echo $response['message']; ?>,
+                icon: <?php echo $response['type']; ?>,
+                confirmButtonColor: '#0174F6'
+            })
+        </script>
+    <?php endif;?>
+
     <form class="container-sm form" method="POST">
         <div class="form__icon">
-            <svg xmlns="http://www.w3.org/2000/svg" height="60" fill="#53d4ff" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" height="60" fill="#ffffff" class="bi bi-envelope-fill" viewBox="0 0 16 16">
                 <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
             </svg>
         </div>
@@ -13,31 +46,29 @@
         
         <div class="form__field">
             <label for="product-name" class="form__label">Nombre del Producto</label>
+            <?php echo isset($errors['product-name']) ? '<p class="form__error">'.$errors['product-name'].'</p>' : '' ?>
             <input id="product-name" name="product-name" type="text" class="form__input" value="<?php echo old("product-name")?>" placeholder="Ingrese el nombre del producto">
         </div>
 
         <div class="form__field">
             <label for="inquirer-name" class="form__label">Nombre del Solicitante</label>
+            <?php echo isset($errors['inquirer-name']) ? '<p class="form__error">'.$errors['inquirer-name'].'</p>' : '' ?>
             <input id="inquirer-name" name="inquirer-name" type="text" class="form__input" value="<?php echo old("inquirer-name")?>" placeholder="Ingrese su nombre">
         </div>
 
         <div class="form__field">
             <label for="inquirer-email" class="form__label">E-Mail</label>
+            <?php echo isset($errors['inquirer-email']) ? '<p class="form__error">'.$errors['inquirer-email'].'</p>' : '' ?>
             <input id="inquirer-email" name="inquirer-email" type="email" class="form__input" value="<?php echo old("inquirer-email")?>" placeholder="Ingrese su correo">
         </div>
 
         <div class="form__field">
             <label for="message" class="form__label">Mensaje</label>
+            <?php echo isset($errors['message']) ? '<p class="form__error">'.$errors['message'].'</p>' : '' ?>
             <textarea id="message" name="message" rows="5" class="form__textarea" placeholder="Ingrese su mensaje"><?php echo old("message")?></textarea>
         </div>
 
-        <div class="form__submit">
-            <input type="submit" value="Enviar">
-            
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#ffffff" class="bi bi-send-fill" viewBox="0 0 16 16">
-                <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z"/>
-            </svg>
-        </div>
+        <input class="form__submit" type="submit" value="Enviar">
     </form>
 </section>
 <?php $this->endSection('content');?>
