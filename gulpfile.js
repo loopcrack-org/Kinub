d47a195dcca1d2729c50560f97e990ecbb69d5d8
Mauricio CR <mauriciocr123@outlook.com>
2023-08-24 17:46:00 -0600

    KIN-186 Adding new gulpfile
    Adding new gulpfile to get more control over compiled styles and javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { task, src, dest, watch, parallel} = require("gulp");
+const { src, dest, watch, parallel} = require("gulp");
 
 //Css
 const sass = require("gulp-sass")(require("sass"));
@@ -18,24 +18,32 @@ const rename = require("gulp-rename");
 const uglify = require('gulp-uglify');
 
 const paths = {
-    src: "./src",
-    scss: {
-        admin: './src/sass/admin/config/app.scss',
-        public: './src/sass/public/app.scss',
+    admin: {
+      scss: {
+        app: './src/sass/admin/config/app.scss',
         bootstrap: './src/sass/admin/config/bootstrap.scss',
+      },
+      js: './src/js/admin/**/*.js',
+      watch: {
+        scss: './src/sass/admin/**/*.scss',
+        js: './src/js/admin/**/*.js',
+      }
     },
-    watchScss: {
-        admin: './src/sass/admin/**/*.scss',
-        public: './src/sass/public/**/*.scss',
+    public: {
+      scss: './src/sass/public/app.scss',
+      js: './src/js/public/**/*.js',
+      watch: {
+        scss: './src/sass/public/**/*.scss',
+        js: './src/js/public/**/*.js',
+      }
     },
-    js: "./src/js/**/*.js",
-    images: "./src/images/**/**",
-    dest: "./public/assets",
+    images: "./src/images/**/*",
+    dest: "./public/assets", 
 };
 
 function publicCss() {
     const destPath = `${paths.dest}/css`;
-    return src(paths.scss.public)
+    return src(paths.public.scss)
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
@@ -46,7 +54,7 @@ function publicCss() {
 
 function adminCss() {
   const destPath = `${paths.dest}/css`;
-  return src(paths.scss.admin)
+  return src(paths.admin.scss.app)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer(), cssnano()]))
@@ -56,7 +64,7 @@ function adminCss() {
 }
 function bootstrap() {
   const destPath = `${paths.dest}/css`;
-  return src(paths.scss.bootstrap)
+  return src(paths.admin.scss.bootstrap)
     .pipe(sourcemaps.init())
     .pipe(sass())
     .pipe(postcss([autoprefixer(), cssnano()]))
@@ -65,8 +73,15 @@ function bootstrap() {
     .pipe(dest(destPath));
 }
 
-function javascript() {
-  return src(paths.js)
+function adminJavascript() {
+  return src(paths.admin.js)
+      .pipe(sourcemaps.init())
+      .pipe(uglify())
+      .pipe(sourcemaps.write("."))
+      .pipe(dest(`${paths.dest}/js`));
+}
+function publicJavascript() {
+  return src(paths.public.js)
       .pipe(sourcemaps.init())
       .pipe(uglify())
       .pipe(sourcemaps.write("."))
@@ -91,17 +106,23 @@ function imageAvif() {
     .pipe(dest(`${paths.dest}/avif`));
 }
 
-function watchFiles() {
-  watch(paths.watchScss.public, publicCss);
-  watch(paths.watchScss.admin, adminCss);
-  watch(paths.js, javascript);
+function watchAdmin() {
+  watch(paths.admin.watch.scss, adminCss);
+  watch(paths.admin.watch.js, adminJavascript);
   watch(paths.images, imagenes);
   watch(paths.images, imageAvif);
   watch(paths.images, imageWebp);
 }
 
-exports.bootstrap = bootstrap;
-exports.css = parallel(adminCss, publicCss);
-exports.javascript = javascript;
-exports.watchFiles = watchFiles;
-exports.default = parallel(adminCss, publicCss, javascript, watchFiles);
+function watchPublic() {
+  watch(paths.public.watch.scss, publicCss);
+  watch(paths.public.watch.js, publicJavascript);
+  watch(paths.images, imagenes);
+  watch(paths.images, imageAvif);
+  watch(paths.images, imageWebp);
+}
+
+exports.bootstrap = bootstrap; // bootstrap
+exports.admin = parallel(adminCss, adminJavascript, watchAdmin); // admin
+exports.default = parallel(publicCss, publicJavascript, watchPublic);  // public
+exports.images = parallel(imagenes, imageWebp, imageAvif); // imagenes
