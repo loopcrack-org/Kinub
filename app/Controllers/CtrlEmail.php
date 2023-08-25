99f899362e3563ea7330cd11ec067b231e2fbf7e
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-08-25 01:38:01 -0600

    KIN-147 Create Controller for Support Form Validation
    The controller to render the technical support page was created, as well as its controller for the validations and its route

--- app/Controllers/CtrlEmail.php
+++ app/Controllers/CtrlEmail.php
@@ -3,17 +3,30 @@
 namespace App\Controllers;
 
 use App\Validation\ContactEmailValidation;
+use App\Validation\SupportEmailValidation;
 use CodeIgniter\HTTP\RedirectResponse;
 
 class CtrlEmail extends BaseController
 {
-    public function sendContactEmail()
+    public function sendContactEmail(): RedirectResponse
     {
         $contactEmailValidation = new ContactEmailValidation();
         $POST = $this->request->getPost();
 
         if(!$contactEmailValidation->validateData($POST)){
             return redirect()->back()->withInput()->with("errors", $contactEmailValidation->getErrors());
+        }
+
+        return redirect()->back();
+    }
+
+    public function sendSupportEmail(): RedirectResponse
+    {
+        $supportEmailValidation = new SupportEmailValidation();
+        $POST = $this->request->getPost();
+
+        if(!$supportEmailValidation->validateData($POST)){
+            return redirect()->back()->withInput()->with("errors", $supportEmailValidation->getErrors());
         }
 
         return redirect()->back();
