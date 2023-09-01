ffd0311055c744fad19000035e1d7d9faaba851a
Carlos May <carlosmay220@gmail.com>
2023-09-01 10:40:11 -0600

    KIN-76 Implemented the footer section

---
+++ app/Views/public/templates/footer.php
@@ -0,0 +1,16 @@
+<footer class="footer">
+    <div class="footer__grid">
+        <div class="footer__advice-container">
+            <a href="#" class="footer__advice">Aviso de privacidad</a>
+        </div>
+
+        <div class="network">
+            <!--The social networks will be gathered from the database-->
+            <a class="network__link" href="https://youtube.com"><span class="network__accessible">YouTube</span></a>
+            <a class="network__link" href="https://linkedin.com"><span class="network__accessible">Linkedin</span></a>
+            <a class="network__link" href="https://twitter.com"><span class="network__accessible">Twitter</span></a>
+            <a class="network__link" href="https://facebook.com"><span class="network__accessible">Facebook</span></a>
+            <a class="network__link" href="https://instagram.com"><span class="network__accessible">Instagram</span></a>
+        </div>
+    </div>
+</footer>
