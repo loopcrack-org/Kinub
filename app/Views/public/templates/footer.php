8b04f3bc111f49179c1abda79da2aa0542bb356b
Carlos May <carlosmay220@gmail.com>
2023-09-01 11:41:56 -0600

    Repair links

--- app/Views/public/templates/footer.php
+++ app/Views/public/templates/footer.php
@@ -7,10 +7,10 @@
         <div class="network">
             <!--The social networks will be gathered from the database-->
             <a class="network__link" href="https://youtube.com" target="_blank"><span class="network__accessible">YouTube</span></a>
-            <a class="network__link" href="https://linkedin.com" target="_blank><span class="network__accessible">Linkedin</span></a>
-            <a class="network__link" href="https://twitter.com" target="_blank><span class="network__accessible">Twitter</span></a>
-            <a class="network__link" href="https://facebook.com" target="_blank><span class="network__accessible">Facebook</span></a>
-            <a class="network__link" href="https://instagram.com" target="_blank><span class="network__accessible">Instagram</span></a>
+            <a class="network__link" href="https://linkedin.com" target="_blank"><span class="network__accessible">Linkedin</span></a>
+            <a class="network__link" href="https://twitter.com" target="_blank"><span class="network__accessible">Twitter</span></a>
+            <a class="network__link" href="https://facebook.com" target="_blank"><span class="network__accessible">Facebook</span></a>
+            <a class="network__link" href="https://instagram.com" target="_blank"><span class="network__accessible">Instagram</span></a>
         </div>
     </div>
 </footer>
