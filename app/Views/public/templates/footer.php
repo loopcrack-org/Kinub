a50ebd055d2b7f05fbbb894d8477dcf37717d924
Carlos May <carlosmay220@gmail.com>
2023-09-02 08:01:23 -0600

    Improved the footer section

--- app/Views/public/templates/footer.php
+++ app/Views/public/templates/footer.php
@@ -5,10 +5,8 @@
         </div>
 
         <div class="network">
-            <!--The social networks will be gathered from the database-->
             <a class="network__link" href="https://youtube.com" target="_blank"><span class="network__accessible">YouTube</span></a>
             <a class="network__link" href="https://linkedin.com" target="_blank"><span class="network__accessible">Linkedin</span></a>
-            <a class="network__link" href="https://twitter.com" target="_blank"><span class="network__accessible">Twitter</span></a>
             <a class="network__link" href="https://facebook.com" target="_blank"><span class="network__accessible">Facebook</span></a>
             <a class="network__link" href="https://instagram.com" target="_blank"><span class="network__accessible">Instagram</span></a>
         </div>
