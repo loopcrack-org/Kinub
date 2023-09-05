4b2d942e2e7d8edb13de63e1365c17b552db86c2
Carlos May <carlosmay220@gmail.com>
2023-09-05 09:03:29 -0600

    Improve the whatsapp button implementation

--- public/assets/js/public/whatsapp.js
+++
@@ -1,4 +0,0 @@
-document.querySelector("#btn-whatsapp").addEventListener("click",e=>{e.preventDefault();window.open(`https://api.whatsapp.com/send/?phone=529993102600&text=
-      *_WHATSAPP KINUB_*%0A
-      *Hola, solicito información sobre dispositivos*`)});
-//# sourceMappingURL=whatsapp.js.map
