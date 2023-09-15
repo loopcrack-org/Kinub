5210bf2c9260b4a408c381ea567ff8ba1778414c
Carlos May <carlosmay220@gmail.com>
2023-09-14 22:20:27 -0600

    KIN-88 Corrected the equipment route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -39,10 +39,7 @@ $routes->get('/', 'CtrlPublicPages::index');
 $routes->get('/soporte', 'CtrlPublicPages::viewSupport');
 $routes->post('/email/contacto', 'CtrlEmail::sendContactEmail');
 $routes->post('/email/soporte', 'CtrlEmail::sendSupportEmail');
-
-
-
-$routes->get('/equipment', 'CtrlPublicPages::viewEquipment');
+$routes->get('/equipos', 'CtrlPublicPages::viewEquipment');
 
 
 /*
