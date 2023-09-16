db42c6ee2dfd7cefb57fe9d8057414b05b57dfa8
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-09-16 03:41:12 -0600

    KIN-84 added real time validation using vanilla validator

--- app/Views/public/support.php
+++ app/Views/public/support.php
@@ -8,6 +8,7 @@
 <!-- JS -->
 <?php $this->section('js'); ?>
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="assets/libs/vanilla-validator/vanilla-validator-concat.min.js"></script>
 <script src="assets/js/public/support.js"></script>
 <?php $this->endSection(); ?>
 
@@ -52,30 +53,28 @@
                 $response = session()->get('response');
                 if(isset($response)):
             ?>
-                <script type="module">
-                    Swal.fire({
-                        title: "<?= $response['title']; ?>",
-                        text: "<?= $response['message']; ?>",
-                        icon: "<?= $response['type']; ?>",
-                        confirmButtonColor: '#0174F6'
-                    });
-                </script>
+            <div id="alert-response"
+                data-title="<?= $response['title']; ?>"
+                data-message="<?= $response['message']; ?>"
+                data-type="<?= $response['type']; ?>"
+                style="display: none;"></div>
             <?php endif;?>
 
             <form class="support-form" id="support-form" action="/email/soporte" method="POST" novalidate> 
-                <fieldset class="support-form__fieldset">
+                <fieldset class="support-form__fieldset support-form__step" data-step="1">
                     <legend class="support-form__legend">Información del Cliente</legend>
 
                     <div class="support-form__field">
                         <label for="support-customer" class="support-form__label">Nombre</label>
                         
-                        <?= isset($errors['support-customer']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-customer'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-customer']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-customer'].'</p>' : '<p class="support-form__error" id="errors-1"></p>' ?>
 
                         <input 
                             id="support-customer"
                             name="support-customer"
                             type="text" 
-                            class="support-form__input" 
+                            class="support-form__input required" 
+                            data-errors-id="errors-1"
                             placeholder="Ingrese su nombre"
                             value="<?php echo old("support-customer")?>"
                         >
@@ -84,13 +83,14 @@
                     <div class="support-form__field">
                         <label for="support-phone" class="support-form__label">Teléfono</label>
                         
-                        <?= isset($errors['support-phone']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-phone'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-phone']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-phone'].'</p>' : '<p class="support-form__error" id="errors-2"></p>' ?>
 
                         <input 
                             id="support-phone"
                             name="support-phone"
                             type="tel" 
-                            class="support-form__input" 
+                            class="support-form__input required phone" 
+                            data-errors-id="errors-2"
                             placeholder="Ingrese su número telefónico"
                             value="<?php echo old("support-phone")?>"
                         >
@@ -99,32 +99,35 @@
                     <div class="support-form__field">
                         <label for="support-email" class="support-form__label">Correo</label>
                         
-                        <?= isset($errors['support-email']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-email'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-email']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-email'].'</p>' : '<p class="support-form__error" id="errors-3"></p>' ?>
 
                         <input 
                             id="support-email"
                             name="support-email"
                             type="email" 
-                            class="support-form__input" 
+                            class="support-form__input required email" 
+                            data-errors-id="errors-3"
                             placeholder="Ingrese su correo"
                             value="<?php echo old("support-email")?>"
                         >
                     </div>
-                    <a class="support-form__btn" id="btn-next-1">Siguiente</a>
+
+                    <span class="support-form__btn next-step">Siguiente</span>
                 </fieldset>
 
-                <fieldset class="support-form__fieldset">
+                <fieldset class="support-form__fieldset support-form__step" data-step="2">
                     <legend class="support-form__legend">Información del Producto</legend>
                     <div class="support-form__field">
                         <label for="support-model" class="support-form__label">Modelo del Producto</label>
                         
-                        <?= isset($errors['support-model']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-model'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-model']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-model'].'</p>' : '<p class="support-form__error" id="errors-4"></p>' ?>
 
                         <input 
                             id="support-model"
                             name="support-model"
                             type="text" 
-                            class="support-form__input" 
+                            class="support-form__input required" 
+                            data-errors-id="errors-4"
                             placeholder="Ingrese el modelo del producto"
                             value="<?php echo old("support-model")?>"
                         >
@@ -133,33 +136,35 @@
                     <div class="support-form__field">
                         <label for="support-serial" class="support-form__label">Número de Serie</label>
                         
-                        <?= isset($errors['support-serial']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-serial'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-serial']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-serial'].'</p>' : '<p class="support-form__error" id="errors-5"></p>' ?>
 
                         <input 
                             id="support-serial"
                             name="support-serial"
                             type="text" 
-                            class="support-form__input" 
+                            class="support-form__input required" 
+                            data-errors-id="errors-5"
                             placeholder="Ingrese el numero de serie"
                             value="<?php echo old("support-serial")?>"
                         >
                     </div>
 
                     <div class="support-form__btns">
-                    <a class="support-form__btn" id="btn-prev-1">Anterior</a>
-                    <a class="support-form__btn" id="btn-next-2">Siguiente</a>
+                        <span class="support-form__btn prev-step">Anterior</span>
+                        
+                        <span class="support-form__btn next-step">Siguiente</span>
                     </div>
                 </fieldset>
 
-                <fieldset class="support-form__fieldset">
+                <fieldset class="support-form__fieldset" data-step="3">
                     <legend class="support-form__legend">Detalles del Problema</legend>
                     
                     <div class="support-form__field">   
                         <label for="support-problem-type" class="support-form__label">Tipo de Problema</label>
                         
-                        <?= isset($errors['support-problem-type']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-problem-type'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-problem-type']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-problem-type'].'</p>' : '<p class="support-form__error" id="errors-6"></p>' ?>
 
-                        <select class="support-form__select" id="support-problem-type" name="support-problem-type">
+                        <select class="support-form__select required" data-errors-id="errors-6" id="support-problem-type" name="support-problem-type">
                             <option class="support-form__option support-form__option--selected" value="" disabled selected>Seleccionar opción</option>
                             <option class="support-form__option" <?php echo (old("support-problem-type") == "1") ?  'selected' : '' ?> value="1">categoria 1</option>
                             <option class="support-form__option" <?php echo (old("support-problem-type") == "2") ?  'selected' : '' ?>  value="2">categoria 2</option>
@@ -171,13 +176,13 @@
                     <div class="support-form__field">
                         <label for="support-problem" class="support-form__label">Problema del Producto</label>
                         
-                        <?= isset($errors['support-problem']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-problem'].'</p>' : '<p class="support-form__error"></p>' ?>
+                        <?= isset($errors['support-problem']) ? '<p class="support-form__error support-form__error--active">'.$errors['support-problem'].'</p>' : '<p class="support-form__error" id="errors-7"></p>' ?>
 
-                        <textarea class="support-form__textarea" id="support-problem" name="support-problem" rows="5" placeholder="Describa el problema del producto"><?php echo old("support-problem")?></textarea>
+                        <textarea class="support-form__textarea required" data-errors-id="errors-7" id="support-problem" name="support-problem" rows="5" placeholder="Describa el problema del producto"><?php echo old("support-problem")?></textarea>
                     </div>
 
                     <div class="support-form__btns">
-                    <a class="support-form__btn" id="btn-prev-2">Anterior</a>
+                    <span class="support-form__btn prev-step">Anterior</span>
                     
                     <input class="support-form__submit" id="btn-submit" type="submit" value="Enviar">
                     </div>
--- src/js/public/support.js
+++ src/js/public/support.js
@@ -1,114 +1,151 @@
-const supportForm = document.querySelector('#support-form');
-const progressName = document.querySelectorAll('.support-progress__name');
-const progressBullet = document.querySelectorAll('.support-progress__bullet');
+document.addEventListener('DOMContentLoaded', function() {
+    const supportForm = document.querySelector('#support-form');
+    const progressName = document.querySelectorAll('.support-progress__name');
+    const progressBullet = document.querySelectorAll('.support-progress__bullet');
 
-const fieldsets = [
-    {
-        nextButton: document.querySelector('#btn-next-1'),
-        fields: [
-            { element: document.querySelector('#support-customer'), errorElement: null },
-            { element: document.querySelector('#support-phone'), errorElement: null },
-            { element: document.querySelector('#support-email'), errorElement: null }
-        ]
-    },
-    {
-        nextButton: document.querySelector('#btn-next-2'),
-        prevButton: document.querySelector('#btn-prev-1'),
-        fields: [
-            { element: document.querySelector('#support-model'), errorElement: null },
-            { element: document.querySelector('#support-serial'), errorElement: null }
-        ]
-    },
-    {
-        prevButton: document.querySelector('#btn-prev-2'),
-        submitButton: supportForm.querySelector('[type="submit"]'),
-        fields: [
-            { element: document.querySelector('#support-problem-type'), errorElement: null },
-            { element: document.querySelector('#support-problem'), errorElement: null }
-        ]
+    const fieldsets = [
+        {
+            fields: [
+                { element: document.querySelector('#support-customer') },
+                { element: document.querySelector('#support-phone') },
+                { element: document.querySelector('#support-email') }
+            ]
+        },
+        {
+            fields: [
+                { element: document.querySelector('#support-model') },
+                { element: document.querySelector('#support-serial') }
+            ]
+        },
+        {
+            fields: [
+                { element: document.querySelector('#support-problem-type') },
+                { element: document.querySelector('#support-problem') }
+            ]
+        }
+    ];
+
+    let currentStep = 1;
+
+    const configSteps = {
+        container: '.support-form__step',
+        button: '.next-step',
+        validationBy: 'onclick',
+        selectors: {
+            error: 'is-invalid',
+            messageError: 'invalid-feedback'
+        },
+        messages: {
+            required: 'campo obligatorio',
+            email: 'correo inválido',
+            phone: 'teléfono inválido'
+        },
+        customViewErrors: {
+            add: function(field, message, cls) {
+                updateErrorContainer(field, message, cls);
+            },
+            remove: function(field, cls) {
+                updateErrorContainer(field, '', cls);
+            }
+        },
+        onContainerSuccess: function(container) {
+            currentStep++;
+            supportForm.style.transform = `translateX(-${(currentStep - 1) * 100}%)`;
+            progressName[currentStep - 2].classList.add('support-progress__name--active');
+            progressBullet[currentStep - 2].classList.add('support-progress__bullet--active');
+        }
+    };
+
+    const validatorSteps = new VanillaValidator(configSteps);
+
+    const configForm = {
+        container: '#support-form',
+        button: '#btn-submit',
+        validationBy: 'onclick',
+        messages: {
+            required: 'campo obligatorio'
+        },
+        customViewErrors: {
+            add: function(field, message, cls) {
+                updateErrorContainer(field, message, cls);
+            },
+            remove: function(field, cls) {
+                updateErrorContainer(field, '', cls);
+            }
+        },
+        onFormSubmit: function(container) {
+            progressName[currentStep - 1].classList.add('support-progress__name--active');
+            progressBullet[currentStep - 1].classList.add('support-progress__bullet--active');
+            document.querySelector('#support-form').submit();
+        }
+    };
+
+    const validatorForm = new VanillaValidator(configForm);
+
+    function updateErrorContainer(field, message, cls) {
+        const containerErrorsID = field.getAttribute('data-errors-id');
+    
+        if (containerErrorsID) {
+            const containerErrors = document.getElementById(containerErrorsID);
+    
+            if (containerErrors) {
+                if (message) {
+                    containerErrors.classList.add('support-form__error--active');
+                    containerErrors.classList.add(cls);
+                    containerErrors.innerHTML = message;
+                } else {
+                    containerErrors.classList.remove('support-form__error--active');
+                    containerErrors.classList.remove(cls);
+                    containerErrors.innerHTML = ''; 
+                }
+            }
+        }
     }
-];
 
-initializeFieldsets();
-
-function initializeFieldsets() {
-    fieldsets.forEach((fieldset, index) => {
-        if (fieldset.nextButton) {
-            fieldset.nextButton.addEventListener('click', () => handleNextButtonClick(index));
-        }
-        if (fieldset.prevButton) {
-            fieldset.prevButton.addEventListener('click', () => handlePrevButtonClick(index));
-        }
-        if (fieldset.submitButton) {
-            supportForm.addEventListener('submit', (e) => handleFormSubmit(e, index));
-        }
-
-        fieldset.fields.forEach((field) => {
-            field.errorElement = field.element.closest('.support-form__field').querySelector('.support-form__error');
-            field.element.addEventListener('keydown', (e) => handleFieldKeydown(e));
+    const prevButtons = document.querySelectorAll('.prev-step');
+    prevButtons.forEach((button) => {
+        button.addEventListener('click', function() {
+            if (currentStep > 1) {
+                currentStep--;
+                supportForm.style.transform = `translateX(-${(currentStep - 1) * 100}%)`;
+                progressName[currentStep - 1].classList.remove('support-progress__name--active');
+                progressBullet[currentStep - 1].classList.remove('support-progress__bullet--active');
+            }
         });
     });
-}
 
-function handleNextButtonClick(index) {
-    const currentFieldset = fieldsets[index];
-    const nextFieldset = fieldsets[index + 1];
+    initializeFieldsets();
 
-    if (!validateFields(currentFieldset.fields)) {
-        return;
+    function initializeFieldsets() {
+        fieldsets.forEach((fieldset) => {
+            fieldset.fields.forEach((field) => {
+                field.element.addEventListener('keydown', (e) => handleFieldKeydown(e));
+            });
+        });
     }
 
-    if (nextFieldset) {
-        supportForm.style.transform = `translateX(-${(index + 1) * 100}%)`;
-        progressName[index].classList.add('support-progress__name--active');
-        progressBullet[index].classList.add('support-progress__bullet--active');
-    }
-}
-
-function handlePrevButtonClick(index) {
-    const prevFieldset = fieldsets[index - 1];
-
-    if (prevFieldset) {
-        supportForm.style.transform = `translateX(-${(index - 1) * 100}%)`;
-        progressName[index - 1].classList.remove('support-progress__name--active');
-        progressBullet[index - 1].classList.remove('support-progress__bullet--active');
-    }
-}
-
-function handleFormSubmit(e, index) {
-    const currentFieldset = fieldsets[index];
-
-    if (!validateFields(currentFieldset.fields)) {
-        e.preventDefault();
-    } else {
-        progressName[index].classList.add('support-progress__name--active');
-        progressBullet[index].classList.add('support-progress__bullet--active');
-        index += 1;
-    }
-}
-
-function handleFieldKeydown(e) {
-    if (e.key === 'Tab') {
-        e.preventDefault();
-    }
-}
-
-function validateFields(fields) {
-    let isValid = true;
-
-    fields.forEach((field) => {
-        const { element, errorElement } = field;
-        const trimmedValue = element.value.trim();
-
-        errorElement.classList.remove('support-form__error--active');
-        errorElement.textContent = '';
-
-        if (trimmedValue === '') {
-            errorElement.classList.add('support-form__error--active');
-            errorElement.textContent = 'Campo Obligatorio';
-            isValid = false;
+    function handleFieldKeydown(e) {
+        if (e.key === 'Tab') {
+            e.preventDefault();
         }
-    });
+    }
 
-    return isValid;
-}
+    displayAlert();
+
+    function displayAlert() {
+        const alertResponse = document.querySelector('#alert-response');
+
+        if (alertResponse) {
+            const title = alertResponse.getAttribute('data-title');
+            const message = alertResponse.getAttribute('data-message');
+            const icon = alertResponse.getAttribute('data-type');
+
+            Swal.fire({
+                icon: icon,
+                title: title,
+                text: message,
+                confirmButtonColor: '#0174F6'
+            });
+        }
+    }
+});
