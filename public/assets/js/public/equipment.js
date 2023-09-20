232b61e2abcfba9f2d51dcdb3f3c4ba7674b760c
Carlos May <carlosmay220@gmail.com>
2023-09-20 00:13:07 -0600

    KIN-88 Improve the sizes, animations and add the flex mixin
    The flex mixin makes easier the cards handling

--- public/assets/js/public/equipment.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener("resize",function(){var e;1024<document.body.clientWidth&&(1!==(e=document.querySelectorAll(".equipment")).length&&4!==e.length&&7!==e.length||console.log("cvhj"))});
-//# sourceMappingURL=equipment.js.map
