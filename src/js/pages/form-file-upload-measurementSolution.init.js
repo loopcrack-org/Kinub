2a7ba1131f2999a406b4a936347f015ac04858f4
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-09-21 00:27:51 -0600

    KIN-207 Create the measurement solutions form
    Measurement Solutions Form Created

---
+++ src/js/pages/form-file-upload-measurementSolution.init.js
@@ -0,0 +1,39 @@
+/*
+Template Name: Velzon - Admin & Dashboard Template
+Author: Themesbrand
+Website: https://Themesbrand.com/
+Contact: Themesbrand@gmail.com
+File: Form file upload Js File
+*/
+
+// FilePond
+FilePond.registerPlugin(
+    // encodes the file as base64 data
+    FilePondPluginFileEncode,
+    // validates the size of the file
+    FilePondPluginFileValidateSize,
+    // corrects mobile image orientation
+    FilePondPluginImageExifOrientation,
+    // previews dropped images
+    FilePondPluginImagePreview,
+    FilePondPluginFileValidateType
+);
+
+
+var inputMultipleElements = document.querySelectorAll('input.filepond-input-multiple');
+if (inputMultipleElements) {
+
+    // loop over input elements
+    Array.from(inputMultipleElements).forEach(function (inputElement) {
+        // create a FilePond instance at the input element location
+        FilePond.create(inputElement, {
+            labelIdle:
+                'Arrastra y suelta tu imagen o  <u style="cursor:pointer;">Selecciona</u>',
+            acceptedFileTypes: ["image/png", "image/jpg", "image/jpeg"],
+            labelFileTypeNotAllowed: "Archivo no valido",
+            fileValidateTypeLabelExpectedTypes: `Se espera {allTypes}`,
+        });
+
+    })
+
+}
