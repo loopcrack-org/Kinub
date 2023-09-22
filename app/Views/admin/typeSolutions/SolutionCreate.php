ce976feb3768f8f0994c588ed8f1e95a61a11f5a
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-09-22 00:20:45 -0600

    KIN-206 Create template for the measurement solutions form
    The template for the measurement solutions form was created

--- app/Views/admin/typeSolutions/SolutionCreate.php
+++ app/Views/admin/typeSolutions/SolutionCreate.php
@@ -26,77 +26,8 @@
                     </a>
                 </div>
                 <form id="createproduct-form" autocomplete="off" class="needs-validation" novalidate method="POST">
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <label class="form-label mb-0" for="category-title-input">
-                                        <h5 class="card-title mb-0">Titulo de la Solución de Medición</h5>
-                                    </label>
-                                </div>
-                                <div class="card-body">
-                                    <div class="hstack gap-3 align-items-start">
-                                        <input type="text" class="form-control" name="name" id="name" value="" placeholder="Ingrese el título" required>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- end col -->
-                    </div>
-                    <!-- end row -->
 
-                    <div class="row">
-                        <div class="col-md-12">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h5 class="card-title mb-0">Descripción de la Solución de Medición</h5>
-                                </div>
-                                <div class="card-body">
-                                    <div class="hstack gap-3 align-items-start">
-                                        <div class="flex-grow-1">
-                                            <textarea class="form-control" rows="3" name="description"></textarea>
-                                        </div>
-                                    </div>
-                                </div>
-                                <!-- end card body -->
-                            </div>
-                            <!-- end card -->
-                        </div>
-                        <!-- end col -->
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title mb-0">Icono</h4>
-                                </div><!-- end card header -->
-
-                                <div class="card-body">
-                                    <input type="file" class="filepond filepond-input-multiple" name="icon" data-allow-reorder="true" data-max-file-size="3MB" data-max-files="3">
-                                </div>
-                                <!-- end card body -->
-                            </div>
-                            <!-- end card -->
-                        </div>
-                        <!-- end col -->
-
-                        <div class="col-md-6">
-                            <div class="card">
-                                <div class="card-header">
-                                    <h4 class="card-title mb-0">Imagen de fondo</h4>
-                                </div><!-- end card header -->
-
-                                <div class="card-body">
-                                    <input type="file" class="filepond filepond-input-multiple" name="image" data-allow-reorder="true" data-max-file-size="3MB" data-max-files="3">
-                                </div>
-                                <!-- end card body -->
-                            </div>
-                            <!-- end card -->
-                        </div>
-                        <!-- end col -->
-                    </div>
-                    <!-- end row -->
+                    <?= $this->include('admin/typeSolutions/solutionsFormTemplate') ?>
 
                     <div class="text-end mb-3">
                         <button type="submit" class="btn btn-primary w-lg">Guardar</button>
