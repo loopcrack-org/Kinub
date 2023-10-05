e7f144db9afccca254d32bb572791b64e4608791
Carlos May <carlosmay220@gmail.com>
2023-10-04 23:55:33 -0600

    KIN-92 Improve grid styles and fix file structure

--- app/Views/public/category.php
+++ app/Views/public/category.php
@@ -1,5 +1,10 @@
 <?php $this->extend('public/templates/layout'); ?>
 
+<?php $this->section('css'); ?>
+<link rel="stylesheet" href="assets/public/css/app.min.css" type="text/css">
+<link rel="stylesheet" href="assets/public/css/category.min.css" type="text/css">
+<?php $this->endSection(); ?>
+
 <?php $this->section('content'); ?>
 <main class="category">
     <div class="category__grid">
@@ -26,7 +31,7 @@
 
         <div class="product">
             <div class="product__image">
-                <img src="assets/images/auth-one-bg.jpg" alt="" class="product__image">
+                <img src="https://cdn-icons-png.flaticon.com/512/6152/6152273.png" alt="" class="product__image">
             </div>
             <div class="product__content">
                 <p class="product__name">E-101</p>
