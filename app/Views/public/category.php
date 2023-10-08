57c89453ecfd934b8e744eb08e16f76c991a01f7
Oswaldo-Chan <92545103+Oswaldo-Chan@users.noreply.github.com>
2023-10-08 05:22:27 -0600

    KIN-92 added filter component
    subtask KIN-263 completed

--- app/Views/public/category.php
+++ app/Views/public/category.php
@@ -2,10 +2,104 @@
 
 <?php $this->section('css'); ?>
 <link rel="stylesheet" href="assets/public/css/app.min.css" type="text/css">
+<link rel="stylesheet" href="assets/public/css/filter.min.css" type="text/css">
 <link rel="stylesheet" href="assets/public/css/category.min.css" type="text/css">
 <?php $this->endSection(); ?>
 
+<?php $this->section('js'); ?>
+<script src="assets/public/js/multiple-options-select.min.js"></script>
+<?php $this->endSection(); ?>
+
 <?php $this->section('content'); ?>
+
+<div class="filter">
+    <input 
+        class="filter__attribute"
+        type="text" 
+        placeholder="Escriba el nombre del producto"
+    >
+
+    <div class="multiple-options-select" data-placeholder="Seleccionar Categorías">
+        <div class="select-btn">
+            <span class="select-btn__btn-text"></span>
+            <span class="select-btn__icon">
+                <img class="select-btn__icon-img" src="assets/images/arrow-down.svg" alt="select icon">
+            </span>
+        </div>
+
+        <ul class="select-list">
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+        </ul>
+    </div>
+
+    <div class="multiple-options-select" data-placeholder="Seleccionar Tags">
+        <div class="select-btn">
+            <span class="select-btn__btn-text"></span>
+            <span class="select-btn__icon">
+                <img class="select-btn__icon-img" src="assets/images/arrow-down.svg" alt="select icon">
+            </span>
+        </div>
+
+        <ul class="select-list">
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+            <li class="select-list__option">
+                <input 
+                    class="select-list__checkbox"
+                    type="checkbox" 
+                    name=""
+                    value=""
+                >
+                <span class="select-list__text">Ejemplo de opción</span>
+            </li>
+        </ul>
+    </div>
+
+    <input type="submit" class="filter__submit" value="Buscar">
+</div>
+
 <main class="category">
     <div class="category__grid">
 
