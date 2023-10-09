f9ceacbba2bb7c8624b01fe8a5b2a35e369bf06b
Luis ]avier Martinez Fernandez <luisjavier004@hotmail.com>
2023-10-09 17:27:43 -0600

    Fixed commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'header-max-length': [2, 'always', 120],
     'header-min-length': [2, 'always', 10],
     'body-leading-blank': [2, 'always'],
-    'subject-case': [
+    'header-case': [
       2,
       'always',
       ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case'],
