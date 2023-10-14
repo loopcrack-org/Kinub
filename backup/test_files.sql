c6e5badf987101b9de1baecd7f0aaa3b10b4fa07
Mauricio CR <mauriciocr123@outlook.com>
2023-10-14 13:20:17 -0600

    change db

--- backup/test_files.sql
+++ backup/test_files.sql
@@ -7,15 +7,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-DROP TABLE IF EXISTS `test_files`;
+CREATE TABLE `test` (
+  `testId` int NOT NULL AUTO_INCREMENT,
+  `testName` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  UNIQUE KEY `id` (`testId`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 CREATE TABLE `test_files` (
   `testFilesId` int NOT NULL AUTO_INCREMENT,
   `fileId` int NOT NULL,
-  `type` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `fileType` enum('image','video','product certificate','datasheet','brochure','user manual') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `testId` int DEFAULT NULL,
+  PRIMARY KEY (`testFilesId`),
   UNIQUE KEY `id` (`testFilesId`),
   KEY `fileId` (`fileId`),
-  CONSTRAINT `test_files_ibfk_1` FOREIGN KEY (`fileId`) REFERENCES `files` (`fileId`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `testId` (`testId`),
+  CONSTRAINT `test_files_ibfk_1` FOREIGN KEY (`fileId`) REFERENCES `files` (`fileId`) ON DELETE CASCADE,
+  CONSTRAINT `test_files_ibfk_2` FOREIGN KEY (`testId`) REFERENCES `test` (`testId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 
 
