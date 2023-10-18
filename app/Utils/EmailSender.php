8c4cbdc6b6e4b4d94d1e58e3527a131c6a0a2709
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-17 21:17:47 -0600

    configuration fixes and logic changes
    the logic was changed so that the html body of the email is now stored and php-cs-fixer when it compiled my php files it found two configuration files and generated an error, so I added a flag so that it finds the project configuration

--- app/Utils/EmailSender.php
+++ app/Utils/EmailSender.php
@@ -1,21 +1,35 @@
 <?php
+
 namespace App\Utils;
 
-use App\Models\EmailModel;
-
-class EmailSender{
-
+class EmailSender
+{
     public static function sendEmail($senderName, $senderEmail, $recipientEmail, $subject, $view, $formData): bool
     {
-
         $email = \Config\Services::email();
         $email->setFrom($senderEmail, $senderName)
             ->setTo($recipientEmail)
             ->setSubject($subject)
             ->setMessage(view($view, [
-                    "formData" => $formData
+                'formData' => $formData,
             ]));
 
         return $email->send();
+    }
+
+    public static function getEmailBody($formData, $viewEmail): string
+    {
+        $emailHTML = view($viewEmail, [
+            'formData' => $formData,
+        ]);
+
+        $emailBody = '';
+        if (preg_match('/<body>(.*?)<\/body>/is', $emailHTML, $matches)) {
+            $emailBody = $matches[1];
+
+            $emailBody = preg_replace('/\s+/', '', $emailBody);
+        }
+
+        return $emailBody;
     }
 }
