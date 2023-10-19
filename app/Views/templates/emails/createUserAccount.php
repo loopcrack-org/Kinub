955bba6c6f2b8dd487afaebb3b3e9f685f0a5499
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-19 02:16:58 -0600

    add white space
    A white space was added between the user's first name and last name

--- app/Views/templates/emails/createUserAccount.php
+++ app/Views/templates/emails/createUserAccount.php
@@ -24,7 +24,7 @@
                                                     <path d="M17.9691 20C17.81 17.1085 16.9247 15 11.9999 15C7.07521 15 6.18991 17.1085 6.03076 20" stroke="#0ab39c" stroke-width="1.5" stroke-linecap="round"/>
                                                     <path d="M7 3.33782C8.47087 2.48697 10.1786 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 10.1786 2.48697 8.47087 3.33782 7" stroke="#0ab39c" stroke-width="1.5" stroke-linecap="round"/>
                                                     </svg>
-                                                    <p style="font-family: 'Roboto', sans-serif; color: #878a99;margin-bottom: 0px; margin-top: 0px;font-weight: 500; line-height: 1.5;"><?= $user["userEmail"];?></p>
+                                                    <p style="font-family: 'Roboto', sans-serif; color: #878a99;margin-bottom: 0px; margin-top: 0px;font-weight: 500; line-height: 1.5;"><?= $user['userEmail']; ?></p>
                                                 </div>
                                             </td>
                                         </tr>
@@ -35,7 +35,7 @@
                                         </tr>
                                         <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                             <td class="content-block" style="font-family: 'Roboto', sans-serif; color: #878a99; box-sizing: border-box; font-size: 15px; vertical-align: top; margin: 0; padding: 0 0 12px; text-align: center;" valign="top">
-                                                <p style="margin: 0 0 0 13px; line-height: 1.5;">Hola <?= $user["userFirstName"] . $user["userLastName"];?>, nos complace informarte que tu cuenta en Kinub ha sido creada exitosamente.</p>
+                                                <p style="margin: 0 0 0 13px; line-height: 1.5;">Hola <?= $user['userFirstName'] . ' ' . $user['userLastName']; ?>, nos complace informarte que tu cuenta en Kinub ha sido creada exitosamente.</p>
                                             </td>
                                         </tr>
                                         <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
@@ -45,12 +45,12 @@
                                         </tr>
                                         <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                             <td class="content-block" itemprop="handler" itemscope="" itemtype="http://schema.org/HttpActionHandler" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 22px; text-align: center;" valign="top">
-                                                <a href="<?=base_url()?>password_reset/<?= $userToken?>" itemprop="url" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: .8125rem; color: #FFF; text-decoration: none; font-weight: 400; text-align: center; cursor: pointer; display: inline-block; border-radius: .25rem; text-transform: capitalize; background-color: #25a0e2; margin: 0; border-color: #25a0e2; border-style: solid; border-width: 1px; padding: .5rem .9rem;">Establecer contraseña</a>
+                                                <a href="<?= base_url()?>password_reset/<?= $userToken?>" itemprop="url" style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: .8125rem; color: #FFF; text-decoration: none; font-weight: 400; text-align: center; cursor: pointer; display: inline-block; border-radius: .25rem; text-transform: capitalize; background-color: #25a0e2; margin: 0; border-color: #25a0e2; border-style: solid; border-width: 1px; padding: .5rem .9rem;">Establecer contraseña</a>
                                             </td>
                                         </tr>
                                         <tr style="font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
                                             <td class="content-block" style="color: #878a99; text-align: center;font-family: 'Roboto', sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top;  border-top: 1px solid #e9ebec; margin: 0; padding: 0; padding-top: 15px" valign="top">
-                                                Si has recibido este correo electrónico por equivocación o no has solicitado la creación de una cuenta de Kinub, ignora este correo. 
+                                                Si has recibido este correo electrónico por equivocación o no has solicitado la creación de una cuenta de Kinub, ignora este correo.
                                             </td>
                                         </tr>
                                     </tbody>
