36ae47b8453b0496b3f96511deaabd3f669f1ae6
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-20 11:27:17 -0600

    remove code that shouldn't be there

--- src/admin/js/alert-deleteElement.js
+++ src/admin/js/alert-deleteElement.js
@@ -1,26 +1,23 @@
-import Swal from "sweetalert2";
+import Swal from 'sweetalert2';
 
 displayAlert();
 function displayAlert() {
-  const alertResponse = document.querySelector("#alert-deletedcategory");
-  const darkMode = document.querySelector("[data-bs-theme]");
+  const alertResponse = document.querySelector('#alert-deletedcategory');
+  const darkMode = document.querySelector('[data-bs-theme]');
 
   if (alertResponse) {
-    const responseData = JSON.parse(
-      alertResponse.getAttribute("data-response")
-    );
+    const responseData = JSON.parse(alertResponse.getAttribute('data-response'));
 
     setTimeout(function () {
-      if (darkMode && darkMode.getAttribute("data-bs-theme") === "dark") {
-        document.querySelector("body").classList.add("darkMode");
-        console.log("hola");
+      if (darkMode && darkMode.getAttribute('data-bs-theme') === 'dark') {
+        document.querySelector('body').classList.add('darkMode');
       }
 
       Swal.fire({
         icon: responseData.type,
         title: responseData.title,
         text: responseData.message,
-        confirmButtonColor: "#0174F6",
+        confirmButtonColor: '#0174F6',
       });
     }, 5);
   }
