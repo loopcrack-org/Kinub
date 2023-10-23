cec70b029d56099e24699d0ba14f329d56dac5a1
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-22 20:22:38 -0600

    parameter name changes

--- app/Controllers/CtrlUser.php
+++ app/Controllers/CtrlUser.php
@@ -2,34 +2,35 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use App\Database\Seeds\EmailSeeder;
 use App\Models\UserModel;
-use App\Utils\EmailSender;
 
 class CtrlUser extends BaseController
 {
     public function viewUsers()
     {
         $userModel = new UserModel();
-        $users = $userModel->findAll();
-        return view("admin/users/Users", ["users" => $users]);
+        $users     = $userModel->findAll();
+
+        return view('admin/users/Users', ['users' => $users]);
     }
+
     public function viewUserCreate()
     {
-        return view("admin/users/UserCreate");
+        return view('admin/users/UserCreate');
     }
+
     public function createUser()
     {
-        $POST = $this->request->getPost();
+        $POST              = $this->request->getPost();
+        $POST['userToken'] = 'FAS12AQajqa';
 
-        return view("templates/emails/createUserAccount", [
-            "user" => $POST,
-            "userToken" => "FAS12AQajqa"
+        return view('templates/emails/createUserAccount', [
+            'formData' => $POST,
         ]);
     }
+
     public function deleteUser()
     {
-        return "deleting user...";
+        return 'deleting user...';
     }
 }
