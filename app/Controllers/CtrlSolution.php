25e6daa9d870b9b5b0590d7ae3b76bf32124a7c0
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-24 03:03:37 -0600

    creating the alert to delete
    The alert to delete the measurement solution was created.

--- app/Controllers/CtrlSolution.php
+++ app/Controllers/CtrlSolution.php
@@ -2,27 +2,50 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-
 class CtrlSolution extends BaseController
 {
     public function viewSolutions()
     {
-        return view("admin/typeSolutions/Solutions");
+        return view('admin/typeSolutions/Solutions');
     }
-    public function viewSolutionCreate() {
-        return view("admin/typeSolutions/SolutionCreate");
+
+    public function viewSolutionCreate()
+    {
+        return view('admin/typeSolutions/SolutionCreate');
     }
-    public function viewSolutionEdit($id) {
-        return view("admin/typeSolutions/SolutionEdit", ["id"=>$id]);
+
+    public function viewSolutionEdit($id)
+    {
+        return view('admin/typeSolutions/SolutionEdit', ['id' => $id]);
     }
-    public function createSolution() {
-        return "creating solution...";
+
+    public function createSolution()
+    {
+        return 'creating solution...';
     }
-    public function updateSolution($id) {
-        return "updating solution... $id";
+
+    public function updateSolution($id)
+    {
+        return "updating solution... {$id}";
     }
-    public function deleteSolution() {
-        return "deleting solution...";
+
+    public function deleteSolution()
+    {
+        $isDeleted = true;
+        if ($isDeleted) {
+            $response = [
+                'title'   => 'Eliminación exitosa',
+                'message' => 'Se ha elimnado la solución de medición correctamente',
+                'type'    => 'success',
+            ];
+        } else {
+            $response = [
+                'title'   => 'Eliminación fallida',
+                'message' => 'No se pudo realizar la eliminación de la solución de medición',
+                'type'    => 'error',
+            ];
+        }
+
+        return redirect()->back()->with('response', $response);
     }
 }
