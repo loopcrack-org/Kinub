63fcbb209c6fec7f754db95d9370a74ad889f0e2
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-10-24 01:36:21 -0600

    add the modal to delete user

--- app/Views/admin/users/Users.php
+++ app/Views/admin/users/Users.php
@@ -71,7 +71,7 @@
                                         <td>
                                             <div class="d-flex justify-content-between">
                                                 <a href="/admin/usuarios/editar/<?= $user['userId']?>" class="btn btn-primary btn-icon waves-effect waves-light" style="width: 48%;"><i class="ri-edit-2-fill ri-lg"></i></a>
-                                                <a href="#" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" style="width: 48%;"><i class="ri-delete-bin-5-line ri-lg"></i></a>
+                                                <a href="#" class="btn btn-danger btn-icon waves-effect waves-light" data-bs-toggle="modal" data-bs-target="#deleteUsersModal" style="width: 48%;"><i class="ri-delete-bin-5-line ri-lg"></i></a>
                                             </div>
                                         </td>
                                     </tr>
@@ -92,6 +92,35 @@
                     <!-- card-body -->
                 </div>
                 <!-- card -->
+
+                <!-- Modal -->
+                <div class="modal fade flip" id="deleteUsersModal" tabindex="-1" aria-hidden="true">
+                    <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="btn-close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <div class="mt-2 text-center">
+                                    <lord-icon src="https://cdn.lordicon.com/tdrtiskw.json" trigger="loop" colors="primary:#25a0e2,secondary:#00bd9d" style="width:130px;height:130px">
+                                    </lord-icon>
+                                    <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
+                                        <h4>¿Estás seguro?</h4>
+                                        <p class="text-muted mx-4 mb-0">
+                                            Eliminar el usuario resultará en la eliminación permanente del elemento. Esta acción no se puede deshacer.</p>
+                                    </div>
+                                </div>
+                                <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
+                                    <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Cerrar</button>
+                                    <form action="/admin/usuarios/borrar" method="post">
+                                        <button type="submit" class="btn w-sm btn-primary" id="delete-record">¡Sí, bórralo!</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--end modal -->
             </div>
             <!-- col -->
         </div>
