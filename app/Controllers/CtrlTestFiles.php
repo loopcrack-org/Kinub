30417ce7c0dfc8233bd5fa274adbcdbc6ce4aae2
Mauricio CR <mauriciocr123@outlook.com>
2023-11-16 23:48:59 -0600

    added file validations

--- app/Controllers/CtrlTestFiles.php
+++ app/Controllers/CtrlTestFiles.php
@@ -6,38 +6,54 @@ class CtrlTestFiles extends CtrlApiFiles
 {
     /**
      * This variable container examples for filepond configuration
-     * for image and viddeo files
+     * for image
      */
     protected $filepondConfig = [
         'baseUrl' => '/admin/testFiles',
         'image'   => [
-            'name'                               => 'image',
-            'acceptedFileTypes'                  => ['image/jpg', 'image/png', 'image/jpeg'],
+            'name' => 'image',
+            // 'acceptedFileTypes'                  => ['image/jpg', 'image/png', 'image/jpeg'],
             'fileValidateTypeLabelExpectedTypes' => 'Selecciona sólo archivos con la extensión .jpg, .jpeg o .png',
             'labelFileTypeNotAllowed'            => 'Archivo no válido',
             'chunkUploads'                       => true,
             'chunkSize'                          => 100000,
             'allowMultiple'                      => true,
-            'maxFiles'                           => 3,
-            'minFiles'                           => 2,
-            'imagePreviewHeight'                 => 170,
-            'imageCropAspectRatio'               => '1:1',
-            'imageResizeTargetWidth'             => 200,
-            'imageResizeTargetHeight'            => 200,
-            'allowFileSizeValidation'            => true,
-            'maxFileSize'                        => '5MB',
-            'minFileSize'                        => '100KB',
-            'labelIdle'                          => 'Arrastra y suelta tus archivos o <span class="filepond--label-action"> Selecciona </span>',
-            'labelFileLoading'                   => 'Cargando',
-            'labelFileProcessing'                => 'Procesando',
-            'labelFileProcessingComplete'        => 'Carga completada',
-            'labelTapToUndo'                     => 'toque para deshacer',
-            'labelTapToCancel'                   => 'toque para cancelar',
-            'labelFileWaitingForSize'            => 'Esperando tamaño',
-            'labelMaxFileSizeExceeded'           => 'El archivo es demasiado grande.',
-            'labelMaxFileSize'                   => 'El tamaño máximo permitido es de {filesize}',
-            'labelMinFileSizeExceeded'           => 'El archivo es demasiado pequeño',
-            'labelMinFileSize'                   => 'El tamaño mínimo permitido es de {filesize}',
+            // 'maxFiles'                           => 3,
+            // 'minFiles'                           => 2,
+            'imagePreviewHeight'      => 170,
+            'imageCropAspectRatio'    => '1:1',
+            'imageResizeTargetWidth'  => 200,
+            'imageResizeTargetHeight' => 200,
+            'allowFileSizeValidation' => true,
+            // 'maxFileSize'                        => '5MB',
+            // 'minFileSize'                        => '100KB',
+            'labelIdle'                   => 'Arrastra y suelta tus archivos o <span class="filepond--label-action"> Selecciona </span>',
+            'labelFileLoading'            => 'Cargando',
+            'labelFileProcessing'         => 'Procesando',
+            'labelFileProcessingComplete' => 'Carga completada',
+            'labelTapToUndo'              => 'toque para deshacer',
+            'labelTapToCancel'            => 'toque para cancelar',
+            'labelFileWaitingForSize'     => 'Esperando tamaño',
+            'labelMaxFileSizeExceeded'    => 'El archivo es demasiado grande.',
+            'labelMaxFileSize'            => 'El tamaño máximo permitido es de {filesize}',
+            'labelMinFileSizeExceeded'    => 'El archivo es demasiado pequeño',
+            'labelMinFileSize'            => 'El tamaño mínimo permitido es de {filesize}',
+        ],
+    ];
+
+    /**
+     * configuration for validation from server through the api
+     */
+    protected $validationConfig = [
+        'image' => [
+            'rules'    => 'maxSize[200,kb]|minSize[1,kb]|maxDims[3000,3000]|mimeIn[image/png,image/jpeg,image/jpg]|extIn[png,jpeg,jpg]',
+            'messages' => [
+                'maxSize' => 'La imagen debe ser menor a 100 kilobytes',
+                'minSize' => 'La imagen debe ser mayor a 1 kilobytes',
+                'maxDims' => 'Las dimensiones máximas de imagen son de 100px por 100px',
+                'mimeIn'  => 'Tipo de archivo inválido, selecciona image/png,image/jpeg,image/jpg',
+                'extIn'   => 'Extensión de archivo inválido, selecciona .png, .jpeg o .jpg',
+            ],
         ],
     ];
 
