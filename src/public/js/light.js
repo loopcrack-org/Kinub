a574fa13758f2a793778f900d6d4f95a22a68424
Carlos May <97546829+CarlosMay7@users.noreply.github.com>
2023-11-19 22:20:52 -0600

    Update light.js

--- src/public/js/light.js
+++ src/public/js/light.js
@@ -2,7 +2,7 @@ import lightGallery from 'lightgallery';
 
 export function initLightGallery() {
   const light = lightGallery(document.querySelector('.glide__slides'), {
-    licenseKey: '927D6AF3-9D4E-4315-A976-33AFB1C334EF',
+    licenseKey: '',
     selector: '.glide__slide',
     controls: false,
   });
