09522977926933bac72d7ac499b75bf1d274699e
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-20 19:48:29 -0600

    do the functionality of creating certificates

--- app/Controllers/CtrlCertificate.php
+++ app/Controllers/CtrlCertificate.php
@@ -3,6 +3,9 @@
 namespace App\Controllers;
 
 use App\Models\CertificateModel;
+use App\Validation\CertificateValidation;
+use Exception;
+use Throwable;
 
 class CtrlCertificate extends BaseController
 {
@@ -28,19 +31,40 @@ class CtrlCertificate extends BaseController
 
     public function createCertificate()
     {
-        $isCreated = true;
-        if ($isCreated) {
-            $response = [
-                'title'   => 'Creación exitosa',
-                'message' => 'Se ha creado el certificado correctamente',
-                'type'    => 'success',
-            ];
-        } else {
-            $response = [
-                'title'   => 'Creación fallida',
-                'message' => 'No se pudo realizar crear el certificado',
-                'type'    => 'error',
-            ];
+        $validateCertificate = new CertificateValidation();
+        $certificateData     = $this->request->getPost(['certificatefileName']);
+
+        try {
+            if (! $validateCertificate->validateInputs($certificateData)) {
+                throw new Exception();
+            }
+
+            $certificateData['certificatePreviewId'] = 1;
+            $certificateData['certificatefileId']    = 1;
+
+            $certificateModel = new CertificateModel();
+            $isCreated        = $certificateModel->insert($certificateData);
+            if ($isCreated) {
+                $response = [
+                    'title'   => 'Creación exitosa',
+                    'message' => 'Se ha creado el certificado correctamente',
+                    'type'    => 'success',
+                ];
+            } else {
+                throw new Exception('Algo salió mal al registrar el certificado. Por favor, inténtalo de nuevo.');
+            }
+        } catch (Throwable $th) {
+            $errors = $validateCertificate->getErrors();
+
+            if (empty($errors)) {
+                $response = [
+                    'title'   => '¡Oops!',
+                    'message' => $th->getMessage(),
+                    'type'    => 'error',
+                ];
+            } else {
+                return redirect()->back()->withInput()->with('errors', $errors);
+            }
         }
 
         return redirect()->to('/admin/certificados')->with('response', $response);
