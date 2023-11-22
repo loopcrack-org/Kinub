dfe77dbaeb5a5a9f7b935e77f6c5bde040d38f33
Alfonso Tort <alfonso.tort@anahuac.com>
2023-11-22 00:34:26 -0600

    form svg select home form behavior fix and responsive changes

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -1,9 +1,30 @@
+import customSelect from 'custom-select';
 import Plyr from 'plyr';
 import Swal from 'sweetalert2';
-import customSelect from 'custom-select';
 
 new Plyr('#kinub-video');
-customSelect('select');
+
+const cstSel = customSelect('select')[0];
+console.log;
+
+cstSel.container.addEventListener('click', function name() {
+  if (!cstSel.container.classList.contains('is-open')) {
+    cstSel.container.classList.add('turn-arrow');
+  } else {
+    cstSel.container.classList.remove('turn-arrow');
+  }
+});
+
+function detectarSelect(e) {
+  e.preventDefault();
+  if (cstSel.container.classList.contains('is-open')) {
+    return;
+  } else {
+    cstSel.container.classList.remove('turn-arrow');
+  }
+}
+
+document.addEventListener('click', detectarSelect);
 
 const showAlert = (props) => {
   Swal.fire({
