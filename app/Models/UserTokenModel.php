5f7870807b74dda4655bb2000520b4cc33cbbc0c
Cristian Pan <a16003566@alumnos.uady.mx>
2023-12-11 02:17:20 -0600

    KIN-290 Add resend email to confirm account functionality

--- app/Models/UserTokenModel.php
+++ app/Models/UserTokenModel.php
@@ -3,6 +3,9 @@
 namespace App\Models;
 
 use CodeIgniter\Model;
+use Exception;
+use Faker\Core\Uuid;
+use Throwable;
 
 class UserTokenModel extends Model
 {
@@ -13,4 +16,34 @@ class UserTokenModel extends Model
         'tokenExpiryDate',
         'userId',
     ];
+
+    public function getNewUserToken(string $userId)
+    {
+        try {
+            $this->db->transStart();
+            $oldTokenDeleted = $this->where('userId', $userId)->delete();
+
+            if (! $oldTokenDeleted) {
+                throw new Exception('Ha ocurrido un error al eliminar el token del usuario');
+            }
+
+            $tokenData = [
+                'userToken'       => substr((new Uuid())->uuid3(), 0, 13),
+                'tokenExpiryDate' => date('y-m-d', strtotime(' +1 day')),
+                'userId'          => $userId,
+            ];
+
+            if ($this->insert($tokenData) === false) {
+                throw new Exception('Ha ocurrido un error al generar el nuevo token');
+            }
+
+            $this->db->transComplete();
+
+            return $tokenData['userToken'];
+        } catch (Throwable $th) {
+            $this->db->transRollback();
+
+            throw $th;
+        }
+    }
 }
