ad098006b9e01a9771fe4e1b9e4c05634e00d4cd
Carlos May <carlosmay220@gmail.com>
2023-12-20 20:39:34 -0600

    First changes

--- app/Views/public/category.php
+++ app/Views/public/category.php
@@ -14,6 +14,30 @@
     <h1 class="jumbotron__title">Telemetría</h1>
 </header>
 
+<section class="search">
+    <form class="search__form" method="post">
+        <input class="search__bar" id="autoComplete" type="search" dir="ltr" spellcheck=false autocorrect="off" autocomplete="off" autocapitalize="off" maxlength="2048" tabindex="1" placeholder="Buscar">
+    </form>
+
+    <div class="search__displays">
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="search__display bi bi-columns-gap" viewBox="0 0 16 16">
+            <path d="M6 1v3H1V1zM1 0a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1zm14 12v3h-5v-3zm-5-1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1zM6 8v7H1V8zM1 7a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V8a1 1 0 0 0-1-1zm14-6v7h-5V1zm-5-1a1 1 0 0 0-1 1v7a1 1 0 0 0 1 1h5a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1z"/>
+        </svg>
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="search__display bi bi-list-nested" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5"/>
+        </svg>
+    </div>
+
+    <div class="search__filters">
+        <button class="search__filter-button">Filtros</button>
+        <select class="search__sorting" name="sorting" id="sorting">
+            <option value="1">De la A-Z</option>
+            <option value="2">De la Z-A</option>
+        </select>
+    </div>
+</section>
+
 <main class="category">
     <div class="category__grid">
 
