9a5cd64bc4270a35c9183823f0d5af2ffa8b4d9a
Mauricio CR <mauriciocr123@outlook.com>
2023-12-23 01:33:38 -0600

    corrections

--- app/Views/admin/products/productsFormTemplate.php
+++ app/Views/admin/products/productsFormTemplate.php
@@ -7,7 +7,7 @@
             </div><!-- end card header -->
 
             <div class="card-body grid row-gap-0 column-gap-0">
-                <div class="g-col-12 g-col-xxl-7">
+                <div class="g-col-12">
                     <!-- Product Name -->
                     <div class="mb-3">
                         <?= validation_show_error('name', 'validationError') ?>
@@ -47,31 +47,28 @@
                     </div>
                 </div>
 
-                <div class="mb-2 g-col-12 g-col-xxl-5">
+                <!-- Carrousel images and videos -->
+                <div class="g-col-12">
                     <!-- Main Image -->
-                    <div class="ps-xxl-3">
-                        <h5 class="fs-14 mb-1">Imagen principal</h5>
-                        <label class="text-muted mb-0">Ingresa la imagen principal</label>
+                    <div class="mb-4">
+                        <h5 class="fs-14 mb-2">Imagen principal</h5>
+                        <label class="text-muted mb-3">Ingresa la imagen principal</label>
                         <?= view('admin/components/inputFilePond', [
                             'config' => $fileConfig['mainImage'],
                         ]) ?>
                     </div>
-                </div>
-
-                <!-- Carrousel images and videos -->
-                <div class="g-col-12">
                     <!-- Images -->
-                    <div class="mb-2">
-                        <h5 class="fs-14 mb-1">Imagenes</h5>
-                        <label class="text-muted mb-0">Ingresa imagenes de galería</label>
+                    <div class="mb-4">
+                        <h5 class="fs-14 mb-2">Imagenes</h5>
+                        <label class="text-muted mb-3">Ingresa imagenes de galería</label>
                         <?= view('admin/components/inputFilePond', [
                             'config' => $fileConfig['image'],
                         ]) ?>
                     </div>
                     <!-- Videos -->
-                    <div class="mb-2">
-                        <h5 class="fs-14 mb-1">Videos</h5>
-                        <label class="text-muted mb-0">Ingresa videos de galería</label>
+                    <div class="mb-4">
+                        <h5 class="fs-14 mb-2">Videos</h5>
+                        <label class="text-muted mb-3">Ingresa videos de galería</label>
                         <?= view('admin/components/inputFilePond', [
                             'config' => $fileConfig['video'],
                         ]) ?>
@@ -92,10 +89,10 @@
 
             <div class="card-body">
                 <!-- Category -->
-                <div class="mb-3">
+                <div class="mb-4">
                     <?= validation_show_error('category', 'validationError') ?>
-                    <h5 class="fs-14">Categoría</h5>
-                    <select id="category" name="category" class="" required>
+                    <h5 class="fs-14 mb-3">Categoría</h5>
+                    <select id="category" name="category" required>
                         <option value="" selected>Selecciona</option>
                         <?php foreach ($categories as $category):?>
                         <option
@@ -108,10 +105,10 @@
                     </select>
                 </div>
                 <!-- Category Tags -->
-                <div class="mb-3">
+                <div class="mb-4">
                     <?= validation_show_error('categoryTags', 'validationError') ?>
-                    <h5 class="fs-14 mb-1">Tags de categoría</h5>
-                    <label class="text-muted">Ingresa tags de la categoría</label>
+                    <h5 class="fs-14 mb-2">Tags de categoría</h5>
+                    <label class="text-muted mb-3">Ingresa tags de la categoría</label>
                     <select id="categoryTags" name="categoryTags[]" class="form-control" multiple required>
                         <option value="">Selecciona</option>
                         <?php foreach (old('categoryTags') ?? [] as $categoryTag):?>
@@ -139,7 +136,7 @@
                 <!-- Product description -->
                 <div class="mb-4">
                     <?= validation_show_error('productDescription', 'validationError') ?>
-                    <h5 class="fs-14">Descripción</h5>
+                    <h5 class="fs-14 mb-2">Descripción</h5>
                     <label class="text-muted mb-3">Ingresa una descripción para el producto</label>
                     <div class="wysiwyg-editor position-relative z-1">
                         <div class="editor"></div>
@@ -148,10 +145,10 @@
                 </div>
 
                 <!-- Product specific information-->
-                <div class="mb-3">
+                <div class="mb-4">
                     <?= validation_show_error('productSpecificInformation', 'validationError') ?>
-                    <h5 class="fs-14 mt-4">Especificaciones técnicas del producto</h5>
-                    <label class="text-muted mb-4">Ingresa información más detallada del producto</label>
+                    <h5 class="fs-14 mb-2">Especificaciones técnicas del producto</h5>
+                    <label class="text-muted mb-3">Ingresa información más detallada del producto</label>
                     <div class="wysiwyg-editor">
                         <div class="editor"></div>
                         <input class="input-wysiwyg" type="hidden" name="productSpecificInformation" value="<?= old('productSpecificInformation') ?? '<h2>Producto de software</h2><p>El mejor producto en el área de software</p><ul><li>Precio</li><li>Calidad</li><li>Rapidez</li></ul>'?>" required >
@@ -159,34 +156,34 @@
                 </div>
 
                 <!-- Download Área -->
-                <div class="mb-3 card-header py-3 px-0">
+                <div class="mb-4 card-header py-3 px-0">
                     <h5>Área de descarga</h5>
                     <label class="text-muted mb-0">Documentos del producto</label>
                 </div>
                 <!-- Brochure -->
-                <div class="mb-3">
-                    <h5 class="fs-14 mb-1">Brochure</h5>
+                <div class="mb-4">
+                    <h5 class="fs-14 mb-3">Brochure</h5>
                     <?= view('admin/components/inputFilePond', [
                         'config' => $fileConfig['brochure'],
                     ]) ?>
                 </div>
                 <!-- Datasheet -->
-                <div class="mb-3">
-                    <h5 class="fs-14 mb-1">Datasheet</h5>
+                <div class="mb-4">
+                    <h5 class="fs-14 mb-3">Datasheet</h5>
                     <?= view('admin/components/inputFilePond', [
                         'config' => $fileConfig['datasheet'],
                     ]) ?>
                 </div>
                 <!-- User Manual -->
-                <div class="mb-3">
-                    <h5 class="fs-14 mb-1">Manual de usuario</h5>
+                <div class="mb-4">
+                    <h5 class="fs-14 mb-3">Manual de usuario</h5>
                     <?= view('admin/components/inputFilePond', [
                         'config' => $fileConfig['userManual'],
                     ]) ?>
                 </div>
                 <!-- Certificate -->
-                <div class="mb-3">
-                    <h5 class="fs-14 mb-1">Certificados</h5>
+                <div class="mb-4">
+                    <h5 class="fs-14 mb-3">Certificados</h5>
                     <?= view('admin/components/inputFilePond', [
                         'config' => $fileConfig['certificate'],
                     ]) ?>
